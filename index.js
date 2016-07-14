8c87cc8b28bfbe274b66f5c0b86f911f8733dfa0
Wilson Young <yyfearth@gmail.com>
2016-07-14 12:44:22 -0700

    make the change to the launcher class to launcher the webview app

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var ScriptBrowser = function (baseBrowserDecorator, script) {
+var WebViewBrowser = function (baseBrowserDecorator) {
   baseBrowserDecorator(this)
 
-  this.name = script
-
-  this._getCommand = function () {
-    return script
+  this._getOptions = function (url) {
+    return [url]
   }
 }
 
-ScriptBrowser.$inject = ['baseBrowserDecorator', 'name']
+WebViewBrowser.prototype = {
+  name: 'WebView',
+
+  DEFAULT_CMD: {
+    darwin: __dirname + '/osx/build/Release/WebViewApp.app/Contents/MacOS/WebViewApp'
+  },
+  ENV_CMD: 'WEBVIEW_BIN'
+};
+
+WebViewBrowser.$inject = ['baseBrowserDecorator']
 
 // PUBLISH DI MODULE
 module.exports = {
-  'launcher:Script': ['type', ScriptBrowser]
+  'launcher:WebView': ['type', WebViewBrowser]
 }
